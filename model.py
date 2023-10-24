4474bcc43bd5b28d4081ef75f8bda1d5c0a20d8b
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-10-24 22:50:32 +0700

    Add model and train

---
+++ model.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import timm
+import pytorch_lightning as pl
+from torch.optim import AdamW
+import evaluate
+
+
+class CustomModel(nn.Module):
+    def __init__(self, model_name, num_classes):
+        super(CustomModel, self).__init__()
+
+        # Load the pre-trained model from 'timm'
+        self.base_model = timm.create_model(model_name, pretrained=True,num_classes=num_classes)
+
+    def forward(self, x):
+        # Forward pass through the base model
+        outputs = self.base_model(x)
+
+        return outputs
+    
+class MyLightningModule(pl.LightningModule):
+    def __init__(self, model , T_max):
+        super(MyLightningModule, self).__init__()
+        self.model = model
+        self.criterion = nn.CrossEntropyLoss()
+        self.metric = evaluate.load("f1")
+        self.save_hyperparameters(ignore=['model'])
+        self.T_max = T_max
+
+    def forward(self, x):
+        # Define the forward pass of your model
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        inputs, targets = batch
+        outputs = self(inputs)
+        loss = self.criterion(outputs, targets)
+        self.log('train_loss', loss)  # Log the loss for visualization
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, targets = batch
+        outputs = self(inputs)
+        loss = self.criterion(outputs, targets)
+        self.log('val_loss', loss)  # Log the validation loss
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=5e-4)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.T_max)
+        return [optimizer], [scheduler]
+
+
---
+++ train.py
@@ -0,0 +1,97 @@
+import pytorch_lightning as pl
+import torch
+import os
+from sklearn.model_selection import StratifiedKFold
+from torchvision import datasets
+import torchvision.transforms as T
+import argparse
+from model import CustomModel, MyLightningModule
+import math
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Your program description here")
+    parser.add_argument("--model_name", default= "hf_hub:timm/tf_efficientnet_b7.ns_jft_in1k", type=str, help="Model name")
+    parser.add_argument("--num_classes", type=int, help="Number of classes")
+    parser.add_argument("--num_epochs", type=int, default=5, help="Number of epochs (default: 5)")
+    parser.add_argument("--base_path", type=str, help="Base path for data")
+    parser.add_argument("--num_folds", type=int, default=5, help="Number of folds for StratifiedKFold (default: 5)")
+    parser.add_argument("--train_batch_size", type=int, default=8, help="Training batch size (default: 8)")
+    parser.add_argument("--eval_batch_size", type=int, default=8, help="Evaluation batch size (default: 8)")
+    parser.add_argument("--image_path", type=str, default='train\\train', help="input image path example train/train")
+    parser.add_argument("--img_size", type=int, default=224, help="Image size (default: 224)")
+    parser.add_argument("--random", type=int, default=42, help="Random stage (default: 42)")
+
+    args = parser.parse_args()
+
+    #Image path 
+    base = os.getcwd()
+    image_path = os.path.join(base,args.image_path)
+
+    print(image_path)
+
+    #Transforms
+    transforms = {
+    "train": T.Compose([
+        T.Resize((args.img_size, args.img_size)),
+        T.ToTensor(),
+        T.Normalize(mean=torch.tensor([0.4850, 0.4560, 0.4060]), std=torch.tensor([0.2290, 0.2240, 0.2250]))
+    ]),
+    "test": T.Compose([
+        T.Resize((args.img_size, args.img_size)),
+        T.ToTensor(),
+        T.Normalize(mean=torch.tensor([0.4850, 0.4560, 0.4060]), std=torch.tensor([0.2290, 0.2240, 0.2250]))
+    ])
+    }
+
+    #Makig datasets
+    dataset = datasets.ImageFolder(root=image_path, # target folder of images
+                                    transform=transforms["train"], # transforms to perform on data (images)
+                                    target_transform=None) # transforms to perform on labels (if necessary)
+
+    # Get the data and labels
+    data = [item[0] for item in dataset.samples]
+    labels = [item[1] for item in dataset.samples]
+
+    #defind Tmax for scheduler 
+    tmax = math.ceil((len(data)/args.num_folds)*(args.num_folds-1))
+
+
+    # Initialize StratifiedKFold
+    skf = StratifiedKFold(n_splits=args.num_folds, shuffle=True, random_state=args.random)
+
+    ## Train loop 
+
+    for fold, (train_idx, val_idx) in enumerate(skf.split(data, labels)):
+
+        # Split data into train and validation sets
+        train_set = torch.utils.data.Subset(dataset, train_idx)
+        val_set = torch.utils.data.Subset(dataset, val_idx)
+
+        # Create data loaders for training and validation
+        train_dataloader = torch.utils.data.DataLoader(train_set, 
+                                                        batch_size=args.train_batch_size, 
+                                                        shuffle=True,)
+        val_dataloader = torch.utils.data.DataLoader(val_set, 
+                                                        batch_size=args.eval_batch_size, 
+                                                        shuffle=False,)
+
+        print(f"Fold {fold+1} of {args.num_folds}")
+
+        # Create a PyTorch Lightning module
+        model = CustomModel(args.model_name, args.num_classes)    
+        lightning_module = MyLightningModule(model,T_max=tmax)
+
+        # Create PyTorch Lightning data loaders
+        train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=args.train_batch_size, shuffle=True)
+        val_dataloader = torch.utils.data.DataLoader(val_set, batch_size=args.eval_batch_size, shuffle=False)
+
+        # Create a PyTorch Lightning trainer
+        trainer = pl.Trainer(max_epochs=args.num_epochs)
+
+        # Train the model
+        trainer.fit(lightning_module, train_dataloader, val_dataloader)
+
+
+if __name__ == '__main__':
+    main()
